0c7fd19601fc20d2bc3ce6a7e61ef02ffa8d0509
María Ripoll Vives <mariaripoll95@icloud.com>
2023-07-28 18:05:14 +0000

    Added the imports and routes to the layout document to bring all the information from the different .js files geenerated

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,6 +5,8 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import { ContactList } from "./views/contactList";
+import { AddContacts } from "./views/addContacts";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -24,6 +26,8 @@ const Layout = () => {
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/demo" element={<Demo />} />
+						<Route path="/contactList" element={<ContactList />} />
+						<Route path="/addContacts" element={<AddContacts />} />
 						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
 					</Routes>
